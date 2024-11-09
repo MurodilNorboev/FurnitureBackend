4555999396a0134ba056f67d120a3b11ed4f88c5
MurodilNorboev <your_email@example.com>
2024-11-09 11:36:24 +0900

    backend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from "express"; // express js !
-import { PORT } from "./utils/secrets.js"; // secret dan olinib kelinyapti !
+import { ENVIROVMENT, PORT } from "./utils/secrets.js"; // secret dan olinib kelinyapti !
 import { Routes } from "./routes/index.js"; // barcha malumotlarni bir joyga jamlangan joydan kelayapti !
 import { MONGODB_CONNECT } from "./utils/datamase.config.js";
 import corst from "cors";
 import { errorMiddleware } from "./middleware/error.middleware.js";
+import { readFileSync } from 'fs';
+import swaggerUI from 'swagger-ui-express';
+
 
 const server = express(); // server chaqirip olindi !
 
@@ -12,6 +15,13 @@ void MONGODB_CONNECT();
 server.use(express.json()); // serverni qayta tasdiqlab olyapmiz !
 server.use(express.urlencoded({ extended: true })); // qayta tasdiqlab olish uchun bolmaasa ya'ni qayta tasdiqlanmasa ishga tushmadi server !
 server.use(corst({}));
+
+if (ENVIROVMENT === 'development') {
+  const apiDocs = JSON.parse(readFileSync(new URL('./swagger.json', import.meta.url)));
+  server.use("/api-docs", swaggerUI.serve, swaggerUI.setup(apiDocs));
+  // console.log(apiDocs);
+  
+}
 
 Routes.forEach((item) => {
   server.use(item.path, item.router); // har birini korib chiqish uchun foreach dan foydalandik !
--- src/utils/secrets.js
+++ src/utils/secrets.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'; // envni import qilip oldik !
 
 dotenv.config(); // bu bolmasa dotenv ishlamaydi !! 
 export const PORT = process.env.PORT || 7070;
-export const ENVIROVMENT = process.env.ENVIROVMENT || 7070;
+export const ENVIROVMENT = process.env.ENVIROVMENT;
 export const REG_KEY = process.env.REG_KEY;
 export const DB_URL = process.env.DB_URL;   
 export const JWT_SECRET = process.env.JWT_SECRET;   
