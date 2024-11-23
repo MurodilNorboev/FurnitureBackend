c3809ddc8233638c83024a28c6dc591d9c1b37b6
MurodilNorboev <your_email@example.com>
2024-11-23 18:29:53 +0900

    backend

--- src/utils/secrets.js
+++ src/utils/secrets.js
@@ -5,7 +5,6 @@ dotenv.config(); // bu bolmasa dotenv ishlamaydi !!
 export const PORT = process.env.PORT || 7070;
 export const ENVIROVMENT = process.env.ENVIROVMENT;
 export const REG_KEY = process.env.REG_KEY;
-export const DB_URL = process.env.DB_URL;   
+export const DB_URL = process.env.DB_URL; 
 export const JWT_SECRET = process.env.JWT_SECRET;   
 
-// console.log(PORT, DB_URL, ENVIROVMENT, JWT_SECRET, REG_KEY);
