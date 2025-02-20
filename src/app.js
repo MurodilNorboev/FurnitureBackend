0a31b6a20a31039e9348f07103be8a514e23a4cf
MurodilNorboev <179554495+MurodilNorboev@users.noreply.github.com>
2025-02-21 00:33:39 +0900

    tuzatish-backent

--- src/app.js
+++ src/app.js
@@ -1,38 +1,48 @@
 import express from "express";
-import { ENVIROVMENT, PORT } from "./utils/secrets.js"; 
-import { Routes } from "./routes/index.js"; 
+import { ENVIROVMENT, PORT } from "./utils/secrets.js";
+import { Routes } from "./routes/index.js";
 import { MONGODB_CONNECT } from "./utils/datamase.config.js";
-import corst from "cors";
+import cors from "cors";
 import { errorMiddleware } from "./middleware/error.middleware.js";
-import { readFileSync } from 'fs';
-import swaggerUI from 'swagger-ui-express';
+import { readFileSync } from "fs";
+import swaggerUI from "swagger-ui-express";
+import { createServer } from "http";
+import { Server as SocketServer } from "socket.io";
 
-const server = express(); 
-void MONGODB_CONNECT();
-server.use(express.json()); 
-server.use(express.urlencoded({ extended: true })); 
-server.use(corst({}));
-server.use(errorMiddleware);
+const app = express(),
+  server = createServer(app),
+  io = new SocketServer(server, {
+    cors: {
+      origin: [
+        "https://my-furniture-store.vercel.app",
+        "https://furnitureadmindashboard.vercel.app",
+        "https://camping-car.vercel.app",
+      ],
+    },
+  });
 
-if (ENVIROVMENT === 'development') {
-  const apiDocs = JSON.parse(readFileSync(new URL('./swagger.json', import.meta.url)));
-  server.use("/api-docs", swaggerUI.serve, swaggerUI.setup(apiDocs));
-}
-
-Routes.forEach((item) => {
-  server.use(item.path, item.router); 
+io.on("connection", (socket) => {
+  console.log("Client connected"),
+    socket.on("updateCart", (data) => io.emit("cartUpdated", data));
+  socket.on("disconnect", () => console.log("Client disconnected"));
 });
 
-server.listen(PORT, () => {
-  console.log(`Server Run ${PORT}!`); 
-}); 
+void MONGODB_CONNECT();
+app.use(express.json(), express.urlencoded({ extended: true }), cors());
 
+if (ENVIROVMENT === "development")
+  app.use(
+    "/api-docs",
+    swaggerUI.serve,
+    swaggerUI.setup(
+      JSON.parse(readFileSync(new URL("./swagger.json", import.meta.url)))
+    )
+  );
 
+Routes.forEach(({ path, router }) => app.use(path, router));
+app.use(errorMiddleware);
 
-
-
-
-
+server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}!`));
 
 // import express from "express";
 // import { ENVIROVMENT, PORT } from "./utils/secrets.js";
@@ -42,40 +52,13 @@ server.listen(PORT, () => {
 // import { errorMiddleware } from "./middleware/error.middleware.js";
 // import { readFileSync } from 'fs';
 // import swaggerUI from 'swagger-ui-express';
-// import http from 'http';  // http server
-// import * as socketIo from 'socket.io';  // socket.io import qilish
 
-// const server = express(); // express serverini chaqirish
-
-// // HTTP serverini yaratish
-// const httpServer = http.createServer(server);
-
-// // Socket.io serverini sozlash
-// const io = new socketIo.Server(httpServer, {  // socket.io serverini sozlash
-//   cors: {
-//     origin: '*', // Agar kerak bo'lsa, CORS sozlamalarini o'zgartirishingiz mumkin
-//   },
-// });
-
-// // Socket.io aloqasini sozlash
-// io.on('connection', (socket) => {
-//   console.log('New client connected');
-
-//   socket.on('updateCart', (data) => {
-//     io.emit('cartUpdated', data); // Barcha mijozlarga xabar yuborish
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('Client disconnected');
-//   });
-// });
-// // console.log(io);
-
+// const server = express();
 // void MONGODB_CONNECT();
-
 // server.use(express.json());
 // server.use(express.urlencoded({ extended: true }));
 // server.use(corst({}));
+// server.use(errorMiddleware);
 
 // if (ENVIROVMENT === 'development') {
 //   const apiDocs = JSON.parse(readFileSync(new URL('./swagger.json', import.meta.url)));
@@ -86,11 +69,6 @@ server.listen(PORT, () => {
 //   server.use(item.path, item.router);
 // });
 
-// server.use(errorMiddleware);
-
-// // HTTP serverni ishga tushirish
-// httpServer.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}!`);
+// server.listen(PORT, () => {
+//   console.log(`Server Run ${PORT}!`);
 // });
-
-
