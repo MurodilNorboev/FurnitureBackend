56c4db322ae26ee498c09b9202c77f37ac73f64e
MurodilNorboev <179554495+MurodilNorboev@users.noreply.github.com>
2025-02-23 13:52:57 +0900

    backentYangilanisihi

--- src/app.js
+++ src/app.js
@@ -1,59 +1,3 @@
-import express from "express";
-import { ENVIROVMENT, PORT } from "./utils/secrets.js";
-import { Routes } from "./routes/index.js";
-import { MONGODB_CONNECT } from "./utils/datamase.config.js";
-import cors from "cors";
-import { errorMiddleware } from "./middleware/error.middleware.js";
-import { readFileSync } from "fs";
-import swaggerUI from "swagger-ui-express";
-import https from "https";
-import { Server as SocketServer } from "socket.io";
-import fs from "fs";
-
-// SSL sertifikatlarini o'qing
-const privateKey = fs.readFileSync("/Users/macbook/Desktop/Backend/backend/ssl-key.pem", "utf8");
-const certificate = fs.readFileSync("/Users/macbook/Desktop/Backend/backend/ssl-cert.pem", "utf8");
-
-// HTTPS serverni yaratish
-const credentials = { key: privateKey, cert: certificate };
-
-const app = express(),
-  server = https.createServer(credentials, app),
-  io = new SocketServer(server, {
-    cors: {
-      origin: [
-        "https://my-furniture-store.vercel.app",
-        "https://furnitureadmindashboard.vercel.app",
-        "https://camping-car.vercel.app",
-      ],
-    },
-  });
-
-io.on("connection", (socket) => {
-  console.log("Client connected"),
-    socket.on("updateCart", (data) => io.emit("cartUpdated", data));
-  socket.on("disconnect", () => console.log("Client disconnected"));
-});
-
-void MONGODB_CONNECT();
-app.use(express.json(), express.urlencoded({ extended: true }), cors());
-
-if (ENVIROVMENT === "development")
-  app.use(
-    "/api-docs",
-    swaggerUI.serve,
-    swaggerUI.setup(
-      JSON.parse(readFileSync(new URL("./swagger.json", import.meta.url)))
-    )
-  );
-
-Routes.forEach(({ path, router }) => app.use(path, router));
-app.use(errorMiddleware);
-
-server.listen(PORT, () => console.log(`✅ Server running on port ${PORT} with HTTPS!`));
-
-
-// io serverni yaratish
 // import express from "express";
 // import { ENVIROVMENT, PORT } from "./utils/secrets.js";
 // import { Routes } from "./routes/index.js";
@@ -62,11 +6,19 @@ server.listen(PORT, () => console.log(`✅ Server running on port ${PORT} with H
 // import { errorMiddleware } from "./middleware/error.middleware.js";
 // import { readFileSync } from "fs";
 // import swaggerUI from "swagger-ui-express";
-// import { createServer } from "http";
+// import https from "https";
 // import { Server as SocketServer } from "socket.io";
+// import fs from "fs";
+
+// // SSL sertifikatlarini o'qing
+// const privateKey = fs.readFileSync("/Users/macbook/Desktop/Backend/backend/ssl-key.pem", "utf8");
+// const certificate = fs.readFileSync("/Users/macbook/Desktop/Backend/backend/ssl-cert.pem", "utf8");
+
+// // HTTPS serverni yaratish
+// const credentials = { key: privateKey, cert: certificate };
 
 // const app = express(),
-//   server = createServer(app),
+//   server = https.createServer(credentials, app),
 //   io = new SocketServer(server, {
 //     cors: {
 //       origin: [
@@ -76,6 +28,7 @@ server.listen(PORT, () => console.log(`✅ Server running on port ${PORT} with H
 //       ],
 //     },
 //   });
+
 // io.on("connection", (socket) => {
 //   console.log("Client connected"),
 //     socket.on("updateCart", (data) => io.emit("cartUpdated", data));
@@ -97,4 +50,51 @@ server.listen(PORT, () => console.log(`✅ Server running on port ${PORT} with H
 // Routes.forEach(({ path, router }) => app.use(path, router));
 // app.use(errorMiddleware);
 
-// server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}!`));
+// server.listen(PORT, () => console.log(`✅ Server running on port ${PORT} with HTTPS!`));
+
+
+// io serverni yaratish
+import express from "express";
+import { ENVIROVMENT, PORT } from "./utils/secrets.js";
+import { Routes } from "./routes/index.js";
+import { MONGODB_CONNECT } from "./utils/datamase.config.js";
+import cors from "cors";
+import { errorMiddleware } from "./middleware/error.middleware.js";
+import { readFileSync } from "fs";
+import swaggerUI from "swagger-ui-express";
+import { createServer } from "http";
+import { Server as SocketServer } from "socket.io";
+
+const app = express(),
+  server = createServer(app),
+  io = new SocketServer(server, {
+    cors: {
+      origin: [
+        "https://my-furniture-store.vercel.app",
+        "https://furnitureadmindashboard.vercel.app",
+        "https://camping-car.vercel.app",
+      ],
+    },
+  });
+io.on("connection", (socket) => {
+  console.log("Client connected"),
+    socket.on("updateCart", (data) => io.emit("cartUpdated", data));
+  socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+void MONGODB_CONNECT();
+app.use(express.json(), express.urlencoded({ extended: true }), cors());
+
+if (ENVIROVMENT === "development")
+  app.use(
+    "/api-docs",
+    swaggerUI.serve,
+    swaggerUI.setup(
+      JSON.parse(readFileSync(new URL("./swagger.json", import.meta.url)))
+    )
+  );
+
+Routes.forEach(({ path, router }) => app.use(path, router));
+app.use(errorMiddleware);
+
+server.listen(PORT, () => console.log(`✅ Server running on port ${PORT}!`));
