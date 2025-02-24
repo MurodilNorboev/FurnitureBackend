0e18ee261ef3e85e927d0f68e01a7429693aa37e
MurodilNorboev <179554495+MurodilNorboev@users.noreply.github.com>
2025-02-24 16:19:40 +0900

    tuzatish

--- src/controllers/upload/upload.controller.js
+++ src/controllers/upload/upload.controller.js
@@ -4,55 +4,55 @@ import { sendFile } from '../../utils/s3.js'
 import { v4 } from 'uuid'
 import path from 'path' 
 
-export const uploadFile = asyncHandler(async (req, res) => {
-  const { image, image1, image2, image3, image4, checkImg } = req.files;
-
-  const filePaths = [];
-
-  const fileGroups = [image, image1, image2, image3, image4, checkImg ];
-  for (const fileGroup of fileGroups) {
-    if (fileGroup) {
-      for (const file of fileGroup) {
-        const key = v4() + path.extname(file.originalname);
-        const filePath = await sendFile(file.buffer, key);
-        filePaths.push(filePath.startsWith('http://') || filePath.startsWith('https://') ? filePath.replace(/^https:\/\//, '') : `https://${filePath}`);
-      }
-    }
-  }
-
-  if (filePaths.length === 0) {
-    return res.status(400).json({
-      success: false,
-      msg: 'At least one image or image1 file is required',
-    });
-  }
-
-  res.status(200).json({ success: true, filePaths });
-});
-
-
 // export const uploadFile = asyncHandler(async (req, res) => {
-//     const { image, image1, image2, image3, image4, checkImg } = req.files;
-  
-//     const filePaths = [];
-  
-//     const fileGroups = [image, image1, image2, image3, image4, checkImg ];
-//     for (const fileGroup of fileGroups) {
-//       if (fileGroup) {
-//         for (const file of fileGroup) {
-//           const key = v4() + path.extname(file.originalname);
-//           const filePath = await sendFile(file.buffer, key);
-//           filePaths.push(filePath.startsWith('https://') ? filePath : `https://${filePath}`);
-//         }
+//   const { image, image1, image2, image3, image4, checkImg } = req.files;
+
+//   const filePaths = [];
+
+//   const fileGroups = [image, image1, image2, image3, image4, checkImg ];
+//   for (const fileGroup of fileGroups) {
+//     if (fileGroup) {
+//       for (const file of fileGroup) {
+//         const key = v4() + path.extname(file.originalname);
+//         const filePath = await sendFile(file.buffer, key);
+//         filePaths.push(filePath.startsWith('http://') || filePath.startsWith('https://') ? filePath.replace(/^https:\/\//, '') : `https://${filePath}`);
 //       }
 //     }
+//   }
+
+//   if (filePaths.length === 0) {
+//     return res.status(400).json({
+//       success: false,
+//       msg: 'At least one image or image1 file is required',
+//     });
+//   }
+
+//   res.status(200).json({ success: true, filePaths });
+// });
+
+
+export const uploadFile = asyncHandler(async (req, res) => {
+    const { image, image1, image2, image3, image4, checkImg } = req.files;
   
-//     if (filePaths.length === 0) {
-//       return res.status(400).json({
-//         success: false,
-//         msg: 'At least one image or image1 file is required',
-//       });
-//     }
+    const filePaths = [];
   
-//     res.status(200).json({ success: true, filePaths });
-//   });
+    const fileGroups = [image, image1, image2, image3, image4, checkImg ];
+    for (const fileGroup of fileGroups) {
+      if (fileGroup) {
+        for (const file of fileGroup) {
+          const key = v4() + path.extname(file.originalname);
+          const filePath = await sendFile(file.buffer, key);
+          filePaths.push(filePath.startsWith('https://') ? filePath : `https://${filePath}`);
+        }
+      }
+    }
+  
+    if (filePaths.length === 0) {
+      return res.status(400).json({
+        success: false,
+        msg: 'At least one image or image1 file is required',
+      });
+    }
+  
+    res.status(200).json({ success: true, filePaths });
+  });
